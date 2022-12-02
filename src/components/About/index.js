08deda665603c43f904f757ff494bed953996f1e
saikrishna4545 <katterasaikrishna@gmail.com>
2022-12-02 04:29:58 +0000

    Routing Practice tests

---
+++ src/components/About/index.js
@@ -0,0 +1,18 @@
+// Write your JS code here
+import './index.css'
+
+const About = () => (
+  <div className="about-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      className="image-size"
+      alt="about"
+    />
+    <h1 className="about-heading">About</h1>
+    <p className="about-description">
+      I love to create! I am a Frontend Web Developer
+    </p>
+  </div>
+)
+
+export default About
